7522e575bfc55e4a981924f555720a41c8785162
Anant Kumar Singh <anantsingh7518935315@gmail.com>
2025-03-10 22:11:04 +0530

    navbar and hero page in trail

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import React from "react";
-import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
 import Portfolio from "./components/Portfolio.jsx";
 import Contact from "./components/Contact.jsx";
 import Login from "./components/Login.jsx";
+import Navbar from "./components/Navbar.jsx";
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <div className="">
+      <Navbar />
       <Hero />
-      <About />
+      {/* 
+      <Hero />
       <Contact />
+      <About />
+     
       <Portfolio />
 
-      <Login />
+      <Login /> */}
     </div>
   );
 }
